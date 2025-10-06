445eba9081d0da54296c175edae8b8922492a79f
Quvonch299 <muminovquvonchbek85@gmail.com>
2025-10-06 20:28:56 +0500

    sd

---
+++ .history/js/app_20251006195213.js
@@ -0,0 +1,8 @@
+<script>
+    const menuBtn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+
+        menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('max-sm:hidden');
+        });
+</script>
